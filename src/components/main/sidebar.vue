5dfa8dd59568fc9e52251a45a83fbddbed56265d
liaoshijung <liaoshijung@163.com>
2018-03-22 10:35:58 +0800

    add scss

--- src/components/main/sidebar.vue
+++ src/components/main/sidebar.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="sidebar" class="sidebar responsive ace-save-state">
+  <div id="sidebar">
     <div class="sidebar-shortcuts">
-      <div style="padding:0 15px" data-toggle="modal" data-target="#rili">
+      <div style="padding:0 15px">
         <div id="oneday" class="oneday" style="font-size:14px!important;"></div>
         <div id="oneyear" class="oneyear" style="font-size:12px!important;"></div>
       </div>
@@ -70,67 +70,67 @@
   }
 </script>
 <style lang="scss">
-.sidebar{
-  background-color: #f2f2f2;
-  border-style: solid;
-  border-color: #CCC;
-  border-width: 0 1px 0 0;
-  width: 160px;
-  float: left;
-  position: static;
-  padding-left: 0;
-  padding-right: 0;
-}
-.sidebar .sidebar-shortcuts{
-  background-color: #fafafa;
-  text-align: center;
-  min-height: 40px;
-  margin-bottom: 0;
-  overflow: hidden;
-  position: relative;
-  border: 0 solid;
-}
-.sidebar .sidebar-toggle{
-  content: "";
-  display: block;
-  border-top: 1px solid #e5e5e5;
-  border-bottom: 1px solid #e5e5e5;
-}
-.sidebar .sidebar-toggle>.fa{
-  margin: 2px 0;
-  padding: 0 5px;
-  line-height: 18px;
-  cursor: pointer;
-  font-size: 14px;
-  border-radius: 100%;
-  border: 1px solid #BBB;
-  position: relative;
-  color: #AAA;
-  background-color: #FFF;
-  text-align: center;
-}
-.nav{
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-  list-style: none;
-}
-.nav-item{
-  display: block;
-  position: relative;
-  float: none;
-  padding: 0;
-  border-style: solid;
-  border-width: 1px 0 0;
-  border-color: #e5e5e5;
-}
-.menu-icon{
-    display: inline-block;
-    min-width: 30px;
-    margin-right: 2px;
-    vertical-align: sub;
-    text-align: center;
-    font-size: 1.4rem;
-    font-weight: normal;
+  #sidebar{
+    background-color: #f2f2f2;
+    border-style: solid;
+    border-color: #CCC;
+    border-width: 0 1px 0 0;
+    width: 160px;
+    float: left;
+    position: static;
+    padding-left: 0;
+    padding-right: 0;
+    .sidebar-shortcuts{
+      background-color: #fafafa;
+      text-align: center;
+      min-height: 40px;
+      margin-bottom: 0;
+      overflow: hidden;
+      position: relative;
+      border: 0 solid;
+    }
+    .sidebar-toggle{
+      content: "";
+      display: block;
+      border-top: 1px solid #e5e5e5;
+      border-bottom: 1px solid #e5e5e5;
+      .fa{
+        margin: 2px 0;
+        padding: 0 5px;
+        line-height: 18px;
+        cursor: pointer;
+        font-size: 14px;
+        border-radius: 100%;
+        border: 1px solid #BBB;
+        position: relative;
+        color: #AAA;
+        background-color: #FFF;
+        text-align: center;
+      }
+    }
+    .nav{
+      box-sizing: border-box;
+      margin: 0;
+      padding: 0;
+      list-style: none;
+    }
+    .nav-item{
+      display: block;
+      position: relative;
+      float: none;
+      padding: 0;
+      border-style: solid;
+      border-width: 1px 0 0;
+      border-color: #e5e5e5;
+    }
+    .menu-icon{
+      display: inline-block;
+      min-width: 30px;
+      margin-right: 2px;
+      vertical-align: sub;
+      text-align: center;
+      font-size: 1.4rem;
+      font-weight: normal;
+    }
   }
 </style>
