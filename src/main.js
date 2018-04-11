5d68196a1357078882580b12b923af4e1870ab48
liaoshijung <liaoshijung@163.com>
2018-04-11 17:33:09 +0800

    add axios

--- src/main.js
+++ src/main.js
@@ -4,11 +4,23 @@ import App from './App.vue'
 import store from './store'
 import router from './router'
 import { DatePicker,Select,Option,Tree,Table,TableColumn,Tabs,TabPane,Message,MessageBox} from 'element-ui';
+import axios from 'axios'
 
 import './css/font.scss'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'element-ui/lib/theme-chalk/index.css';
+
+const instance = axios.create({
+  baseURL: 'http://172.10.60.61:8083/',
+  timeout: 10000,
+  headers: {
+    type: 'APP',
+    nonce: 0,
+    loc: 'zh_CN',
+    'Content-type': 'application/json;charset=utf-8'
+  }
+});
 
 Vue.use(BootstrapVue)
 Vue.use(DatePicker)
@@ -23,6 +35,7 @@ Vue.config.productionTip = false
 Vue.prototype.$message = Message;
 Vue.prototype.$alert = MessageBox.alert;
 Vue.prototype.$confirm = MessageBox.confirm;
+Vue.prototype.$http = instance;
 
 let sc = new Vue({
   el: '#app',
