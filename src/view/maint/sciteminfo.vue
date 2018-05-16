cf7e5546f073c748acd38f81d52535ab7e35413e
zz <zhzh87526@163.com>
2018-05-16 11:26:56 +0800

    Merge branch 'master' of https://github.com/zzhen123/Vue-WebSC
    # Conflicts:
    #	package-lock.json
    #	src/components/remark.vue
    #	src/router/index.js
    #	src/store/index.js
    #	src/store/modules/scnote.js
    #	src/view/CaterList.vue
    #	src/view/catering/CateringInfo.vue
    #	src/view/maint/pccodeinfo.vue

--- src/view/maint/sciteminfo.vue
+++ src/view/maint/sciteminfo.vue
@@ -296,7 +296,7 @@
         currentRow: null,
         placeRow: null,
         // 要展开的行，数值的元素是row的key值
-        tableHeight: document.body.clientHeight-140,//减去header的60px
+        tableHeight: document.body.clientHeight-205,//减去header的60px
         num:0,
       }
     },
