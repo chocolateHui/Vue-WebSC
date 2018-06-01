5fd91ed528abbfb878f18cdb5764994af6157196
liaoshijung <liaoshijung@163.com>
2018-06-01 17:21:48 +0800

    新建宴会预订调整

---
+++ src/view/catering/NewCateringQuery.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <CateringInfo ref="newcatering" isNew @saveCatering="saveCatering"></CateringInfo>
+    <NewEvent isNew :toggleshow="toggleshow" :eventshow="eventshow" ref="newevent"></NewEvent>
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue'
+  import { mapGetters, mapMutations } from 'vuex'
+  import CateringInfo from '../../components/catering/CateringInfo.vue'
+  import NewEvent from '../../components/catering/NewEvent.vue'
+  import cateringMixin from './cateringMixin'
+  import '../../css/font.scss'
+
+  export default {
+    data(){
+      return {
+
+      }
+    },
+    components: {
+      CateringInfo,
+      NewEvent
+    },
+    mixins: [cateringMixin]
+  }
+</script>
+<style lang="scss">
+  .card {
+    margin-bottom: 15px;
+    .form-control{
+      height: 33.5px;
+    }
+    .ivu-input{
+      height: 30px;
+      margin-top: 0;
+    }
+  }
+</style>
