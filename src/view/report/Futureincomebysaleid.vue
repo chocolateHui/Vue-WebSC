26748f1e37467d72c392828df98208530a636bdd
zz <zhzh87526@163.com>
2018-06-07 16:04:51 +0800

    Merge branch 'master' of https://github.com/zzhen123/Vue-WebSC
    # Conflicts:
    #	package-lock.json
    #	src/components/remark.vue
    #	src/router/index.js
    #	src/store/index.js
    #	src/store/modules/scnote.js
    #	src/view/CaterList.vue
    #	src/view/catering/CateringInfo.vue
    #	src/view/maint/pccodeinfo.vue

---
+++ src/view/report/Futureincomebysaleid.vue
@@ -0,0 +1,309 @@
+<!-- 模板组件，用于模拟不同路由下的组件显示 -->
+<template>
+  <div id="Lossstatistics">
+    <b-container fluid>
+      <!-- User Interface controls -->
+      <b-row>
+        <b-col sm="4" class="my-1">
+          <el-date-picker
+            v-model="reportdate"
+            value-format="yyyy-MM-dd"
+            type="daterange"
+            range-separator="至"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期">
+          </el-date-picker>
+        </b-col>
+        <b-col sm="4" class="my-1">
+          <b-form-group horizontal label="销售员" class="mb-0">
+            <el-select v-model="saleid" clearable filterable placeholder="请选择">
+              <el-option
+                v-for="item in eloptions"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+            </el-select>
+          </b-form-group>
+        </b-col>
+        <b-col sm="4" class="my-1">
+          <b-form-group class="mb-0">
+            <b-button @click="getreportdata" variant="primary">查询</b-button>
+            <b-button @click="exportexcel" variant="success">导出</b-button>
+          </b-form-group>
+        </b-col>
+      </b-row>
+      <label v-if="!reportdate">请选择报表开始和结束日期</label>
+      <label v-else>
+        <span>开始日期:{{reportdate[0]}}</span>
+        <span> 结束日期:{{reportdate[1]}}</span>
+        <span>销售员:{{saleid}}</span>
+      </label>
+      <el-table
+        id="datatable"
+        ref="datatable"
+        border
+        show-summary
+        :data="searchitems"
+        :summary-method="getSummaries"
+        :row-class-name="tableRowClassName"
+        style="width: 100%" :max-height="tableHeight">
+        <el-table-column
+          v-for="item in fi"
+          :prop="item.prop"
+          :label="item.label"
+          :width="item.width"
+          :sortable="item.sortable"
+          :show-overflow-tooltip="item.showTip" :key="item.prop">
+        </el-table-column>
+      </el-table>
+    </b-container>
+  </div>
+</template>
+
+<script>
+  import XLSX from 'xlsx'
+  import Vue from 'vue'
+  import { mapGetters, mapMutations } from 'vuex'
+  import methodinfo from '../../config/MethodConst'
+
+  var items = []
+  const fildes = [
+    {  prop: 'caterid', label:  '订单编号',width:'138',sortable:false ,showTip:true},
+    {  prop: 'caterdes', label:  '订单名称',width:'',sortable:false,showTip:true},
+    {  prop: 'arr', label:  '到店时间',width:'100',sortable:false,showTip:true },
+    {  prop: 'dep', label:  '离店时间',width:'100',sortable:false,showTip:true },
+    {  prop: 'sta', label:  '状态',width:'100',sortable:false ,showTip:true},
+    {  prop: 'cusnodes', label:  '协议单位',width:'',sortable:false,showTip:true},
+    {  prop: 'saleid', label:  '销售员编号',width:'100',sortable:false ,showTip:true},
+    {  prop: 'salename', label:  '销售员',width:'90',sortable:false ,showTip:true},
+  ]
+
+  export default {
+    data () {
+      return {
+        items:[],
+        fi :fildes,
+        totalRows: items.length,
+        sortBy: null,
+        sortDesc: false,
+        saleid: '',
+        eloptions: [],
+        reportdate: '',
+        begina:'',
+        tableHeight: document.body.clientHeight-190,//减去header的60px
+      }
+    },
+    props:['begin','sale'],
+    computed: {
+      ...mapGetters([
+        'salelist',
+      ]),
+      searchitems:function () {
+        if(!this.saleid){
+          return this.items;
+        }else{
+          return this.items.filter( item => (~item.saleid.indexOf(this.saleid)));
+        }
+      }
+    },
+    methods: {
+      exportexcel:function () {
+        //跳过json的key,手工添加输出标题
+        let sheetopt = {skipHeader:true};
+        let exportitems = this.items.slice(0);
+        exportitems.unshift({caterid: '订单编号', name:  '订单名称'});
+        //json转换为表格
+        let worksheet = XLSX.utils.json_to_sheet(exportitems,sheetopt);
+        worksheet['!cols'] = [{width:10},{width:30}]
+        let new_workbook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(new_workbook, worksheet, "Sheet1");
+        /* generate file and force a download*/
+        XLSX.writeFile(new_workbook, "sheetjs.xlsx");
+      },
+      getsaleid(){
+        this.$store.dispatch('encrypttoken').then(() => {
+          this.$store.dispatch("getSale");
+        });
+      },
+      getreportdata(){
+        if(!this.reportdate){
+          this.$message.error({
+            message:"请选择日期"
+          });
+        }
+        else{
+          let bdate = this.reportdate[0];
+          let edate = this.reportdate[1];
+          this.$store.dispatch('encrypttoken').then(() => {
+            this.$http.defaults.headers.common['username'] = this.$store.getters.username
+            this.$http.defaults.headers.common['signature'] = this.$store.getters.signature
+            this.$http.defaults.headers.common['timestamp'] = new Date().getTime();
+            this.$http.post(methodinfo.getsaleincomereportlist, {
+              bdate:bdate,
+              edate:edate
+            }).then((response)=> {
+              if (response.data.errorCode=="0") {
+                if(typeof(response.data.fileds) != "undefined"){
+                  this.items = [];
+                  this.fi = Object.assign([],fildes);
+                  for(let items of response.data.fileds){
+                    let types = {};
+
+                    types["prop"]=items.props;
+                    if(items.props===""){
+                      types["prop"] = "zzzz";
+                    }
+                    types["label"]=items.label;
+                    this.fi.push(types);
+                  }
+                  let types = {};
+                  types["prop"]="heji";
+                  types["label"]="合计";
+                  this.fi.push(types);
+                  for(let items of response.data.incomes){
+                    let types = {};
+                    types["caterid"]=items.caterid;
+                    types["caterdes"]=items.caterdes;
+                    types["sta"]=items.sta;
+                    types["arr"]=items.arr;
+                    types["dep"]=items.dep;
+                    types["cusnodes"]=items.cusnodes;
+                    types["saleid"]=items.saleid;
+                    types["salename"]=items.salename;
+                    types["heji"]=items.heji;
+                    let m = JSON.parse(items.incomejson);
+                    for(let s in m){
+                      if(s===""){
+                        types["zzzz"]=m[s];
+                      }
+                      else{
+                        types[s]=m[s];
+                      }
+
+                    }
+                    this.items.push(types)
+                  }
+                }
+              }
+              else{
+                this.items = [];
+              }
+            })
+          })
+        }
+      },
+      getSummaries(param) {
+
+        const { columns, data } = param;
+        const sums = [];
+        columns.forEach((column, index) => {
+          if (index === 0) {
+            sums[index] = '合计';
+            return;
+          }
+          if (index >= 1&&index <= 5) {
+            sums[index] = "";
+            return;
+          }
+          if (index === 6||index === 7) {
+            sums[index] = data.length;
+            return;
+          }
+          const values = data.map(item => Number(item[column.property]));
+          if (!values.every(value => isNaN(value))) {
+            sums[index] = values.reduce((prev, curr) => {
+              const value = Number(curr);
+              if (!isNaN(value)) {
+                return prev + curr;
+              } else {
+                return prev;
+              }
+            }, 0);
+            sums[index] += '';
+          } else {
+            sums[index] = 'N/A';
+          }
+        });
+
+        return sums;
+      },
+      tableRowClassName({row, rowIndex}) {
+        if (row.caterid === '小计') {
+          return 'warning-row';
+        } else if (row.caterid === '合计') {
+          return 'success-row';
+        }
+        return '';
+      }
+    },
+    watch:{
+      salelist(val,oldval){
+        for(let elm of val ){
+          let type = {};
+          type["value"] = elm.code;
+          type["label"] = elm.name;
+          this.eloptions.push(type);
+        }
+      },
+      begina(val,oldval){
+        if(val==="xxxx"||val===""){
+        }
+        else{
+          let date = [];
+          date[0] = val;
+          date[1] = val;
+          this.reportdate = date;
+          this.saleid = this.sale;
+          this.getreportdata()
+        }
+
+      },
+    },
+    created(){
+      this.getsaleid();
+      this.begina = this.begin;
+    }
+  }
+</script>
+<style lang="scss"  type="text/scss">
+  #Lossstatistics{
+    .el-date-editor .el-range-separator{
+      padding: 0;
+    }
+    caption{
+      caption-side: top;width: 100%;
+      span{
+        padding-left:5px
+      }
+    }
+    .btn{
+      width: 100px;
+    }
+    .table{
+      border-color: #dee2e6;
+      th{
+        border-color: #dee2e6;
+      }
+    }
+    .el-input__inner{
+      height: 36px;
+    }
+    .el-table td, .el-table th{
+      padding: 0;
+    }
+    .el-table .caret-wrapper{
+      width: 20px;
+    }
+    .row{
+      margin-right: 0;
+    }
+    .el-table .warning-row {
+      background: #e5e5e5;
+    }
+
+    .el-table .success-row {
+      background: #CCC;
+    }
+  }
+</style>
