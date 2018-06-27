f3a2f19348e4888e3bde25a39b0faf65348688d1
bby <1305245477@qq.com>
2018-06-27 10:22:03 +0800

    测试页面修改

--- src/view/maint/setjob.vue
+++ src/view/maint/setjob.vue
@@ -222,7 +222,6 @@
               }else{
                 _this.hotelData=[]
               }
-              console.log(JSON.stringify(this.hotelData)+'pp')
             }
           })
         })
